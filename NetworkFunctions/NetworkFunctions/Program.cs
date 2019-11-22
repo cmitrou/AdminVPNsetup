ade10905441b093b6d9c60a7dce34d44d047e746
Christos D. Mitrou <cmitrou@hotmail.gr>
2019-11-22 22:14:13 +0200

    wqw

--- NetworkFunctions/NetworkFunctions/Program.cs
+++ NetworkFunctions/NetworkFunctions/Program.cs
@@ -25,22 +25,44 @@ namespace NetworkFunctions
             {
                 Console.WriteLine("Name: {0}", adapter.Name);
                 Console.WriteLine("Name: {0}", adapter.Id);
-                netw_card = adapter.Description +" "+ adapter.Id;
-                byte[] idbuffer = Encoding.UTF8.GetBytes(adapter.Id.Trim());
-                using (SHA1Managed sha1 = new SHA1Managed())
-                {
-                    var hash = sha1.ComputeHash(idbuffer);
-                    var sb = new StringBuilder(hash.Length);
-                    foreach (byte b1 in hash)
-                    {
+                netw_card = adapter.Description + " " + adapter.Id;
+                string crd_id = adapter.Id.Trim(new Char[] { '{', '}' });
+                crd_id = (crd_id.Remove(8, 1).Remove(12, 1).Remove(16, 1).Remove(20, 1));
+                Console.WriteLine(crd_id);
 
-                        sb.Append(b1.ToString());
-                    }
-                   string crdid1 = sb.ToString();
-                    string crdid = Encoding.UTF8.GetString(idbuffer, 0, idbuffer.Length);
-                    Console.WriteLine("Hashed" , crdid1);
+                //      //  byte[] idbuffer1 = Encoding.UTF8.GetBytes(adapter.Id.Trim());
+                //      // using (SHA1Managed sha1 = new SHA1Managed().HashSize)
+                //      int DATA_SIZE = 20;
+                //      byte[] idbuffer1 = (Encoding.UTF8.GetBytes(crd_id));
+                ////      idbuffer1 = new byte[DATA_SIZE];
+                //      byte[] idbuffer =  new byte[DATA_SIZE];
+                ////      idbuffer = idbuffer1;
+                //      byte[] reslt;
+                //      SHA1 sha = new SHA1CryptoServiceProvider();
+                //      reslt = sha.ComputeHash(idbuffer1);
+                // //     if (BitConverter.IsLittleEndian)
+                //  //        Array.Reverse(reslt);
+                //      Int32 i = BitConverter.ToInt32(reslt, 0);
+                //      Console.WriteLine("int: {0}", i);
+                ////     string crdid1 = sb.ToString();
+                //     string crdid = Encoding.UTF8.GetString(reslt, 0, reslt.Length);
+                //      Console.WriteLine(crdid.Trim());
+                ////  }
+                ///
+                var data = Encoding.ASCII.GetBytes(crd_id);
+                byte[] hasData = new SHA1Managed().ComputeHash(data);
+                var hash = string.Empty;
+                foreach (var b in hasData)
+                    
+                {
+                    hash += b.ToString("X2");
                 }
-                    Console.WriteLine(netw_card);
+                Console.WriteLine(hash);
+                if (BitConverter.IsLittleEndian)
+                    Array.Reverse(hasData);
+                    Int32 i = BitConverter.ToInt32(hasData, 0);
+                Console.WriteLine("int: {0}", i);
+                Console.WriteLine(netw_card);
                 Console.WriteLine(adapter.Description);
                 Console.WriteLine(String.Empty.PadLeft(adapter.Description.Length, '='));
                 Console.WriteLine("  Interface type .......................... : {0}", adapter.NetworkInterfaceType);
