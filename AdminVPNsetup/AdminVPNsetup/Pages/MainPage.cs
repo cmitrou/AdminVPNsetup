bda667d8175e04925a977fc28e069aabdf2057d7
Christos D. Mitrou <cmitrou@hotmail.gr>
2019-11-27 20:19:13 +0200

    menu page1

--- AdminVPNsetup/AdminVPNsetup/Pages/MainPage.cs
+++ AdminVPNsetup/AdminVPNsetup/Pages/MainPage.cs
@@ -10,7 +10,7 @@ namespace AdminVPNsetup.Pages
     class MainPage : MenuPage
     {
         public MainPage(EasyConsole.Program menuprog) : base("Main Page",menuprog,
-            new Option("Bridge Setup Check", () => AdminVPNsetup.Program._Check_Installation()),
+            new Option("VPN Server Bridge Setup Check", () => menuprog.NavigateTo<Page1>()),
             new Option("Bridge Capable Network Cards", () => AdminVPNsetup.Program._PrintBridgeCapableList()),
             new Option("Input", () => menuprog.NavigateTo<InputPage>()))
         { }   
--- AdminVPNsetup/AdminVPNsetup/Pages/Page1.cs
+++ AdminVPNsetup/AdminVPNsetup/Pages/Page1.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EasyConsole;
 
 namespace AdminVPNsetup.Pages
 {
-    class Page1
+    class Page1 : MenuPage
     {
+        public Page1(EasyConsole.Program program) : base("Page1", program)
+        {
+        }
+        public override void Display()
+        {
+            base.Display();
+            Output.WriteLine("Hello from Page 1Ai");
+
+            Input.ReadString("Press [Enter] to navigate home");
+            Program.NavigateHome();
+        }
     }
 }
