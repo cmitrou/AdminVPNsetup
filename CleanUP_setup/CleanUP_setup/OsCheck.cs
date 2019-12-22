b4ccd143e5f7deb6126f3249aa074e6da8781b37
Christos D. Mitrou <cmitrou@hotmail.gr>
2019-12-22 15:32:01 +0200

    clean_up

--- CleanUP_setup/CleanUP_setup/OsCheck.cs
+++ CleanUP_setup/CleanUP_setup/OsCheck.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// Os Check if 32bit or 64bit
 namespace CleanUP_setup
 {
     class OsCheck
